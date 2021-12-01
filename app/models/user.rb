445d3b2ccf9138578befa09b3f8f5c070694c133
Osman Fatih DEMİR <unssenpai@gmail.com>
2021-12-01 12:06:23 +0300

    Add validation

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# email:string
+# password_digest:string
+# 
+# password:string virtutal
+# password_confirmation:string virtual
+class User < ApplicationRecord
+    has_secure_password
+    validates :email,presence :true,format:{with:/\A[^@\s]+@[^@\s]+\z/, message:"must be a invalid email adress"}
+end
