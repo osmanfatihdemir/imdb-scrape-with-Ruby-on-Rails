9e2814543d681b771b2e199fe1cebe1dbf51fcd5
Osman Fatih DEMİR <unssenpai@gmail.com>
2021-12-02 19:18:11 +0300

    Sing in, sing up, change password

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,11 @@
 class MainController<ApplicationController
     def index
-        flash[:notice]="Logged in successfully"
-        flash[:alert]="Invalid email or password"
+        if session[:user_id]
+            @user=User.find_by(id: session[:user_id])
+        end  
+        
+        #flash[:notice]="Logged in successfully"
+        #flash[:alert]="Invalid email or password"
     end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@
 # password_confirmation:string virtual
 class User < ApplicationRecord
     has_secure_password
-    validates :email,presence :true,format:{with:/\A[^@\s]+@[^@\s]+\z/, message:"must be a invalid email adress"}
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    #validates :email, presence :true, format:{with:/\A[^@\s]+@[^@\s]+\z/, message:"must be a invalid email adress"}
 end
